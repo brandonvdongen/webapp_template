119ba794e670fb3fd745929cdff5584f84cb9c18
brandon van dongen <brandonvdongen@hotmail.com>
2018-02-26 10:01:42 +0100

    moved some things around for cleanup

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,20 +1,43 @@
 module.exports = function (grunt) {
+    require('load-grunt-tasks')(grunt);
     grunt.initConfig({
         uglify: {
+            options: {
+                mangle: false
+            },
             my_target: {
                 files: {
-                    'dest/js/core.min.js': ['js/*.js']
+                    'live/js/core.min.js': ['js/*.js']
                 }
+            }
+        },
+        babel: {
+            options: {
+                sourceMap: true,
+                presets: ['env']
+            },
+            dist: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: 'src/',
+                        src: ["js/*.js"],
+                        dest: 'dist/'
+                    }
+                ]
             }
         }
     });
 
+
     grunt.loadNpmTasks('grunt-contrib-uglify');
 
     grunt.registerTask('build', [
+        'babel',
         'uglify'
     ]);
 
 
     grunt.registerTask('default', ['build']);
-};
+}
+;
